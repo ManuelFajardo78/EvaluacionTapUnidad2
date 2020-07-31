370031c857684f22e59d89a3f1cf224e3d6dcfa4
ManuelFajardo78 <57449214+ManuelFajardo78@users.noreply.github.com>
2020-07-31 16:11:07 -0500

    adicionando el crossoriginss

--- evaluacion/src/main/java/com/example/evalucacion/controller/AlumnoControl.java
+++ evaluacion/src/main/java/com/example/evalucacion/controller/AlumnoControl.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200", maxAge =3200 )
 @RequestMapping("/api/vl")
 public class AlumnoControl {
      @Autowired
