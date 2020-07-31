7b0f6cc79f1bd56eaef37efbf369c7962efe0bdc
fxvr15vasquez <57450853+fxvr15vasquez@users.noreply.github.com>
2020-07-31 16:55:47 -0500

    punto

--- evaluacion/src/main/java/com/example/evaluacion/controller/AlumnoController.java
+++ evaluacion/src/main/java/com/example/evaluacion/controller/AlumnoController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200", maxAge =3200 )
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3200 )
 @RestController
 @RequestMapping("/api/vl")
 public class AlumnoController {
